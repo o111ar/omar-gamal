6b1019cd29f99880b96be6820c63071e941ff64e
Omar <93974776+o111ar@users.noreply.github.com>
2022-11-21 23:32:52 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const btnEl = Omardocument.querySelector(".btn");
+
+btnEl.addEventListener("mouseover", (event) => {
+    const x = event.pageX - btnEl.offsetLeft;
+    const y = event.pageY - btnEl.offsetTop;
+
+    btnEl.style.setProperty("--xPos", x + "px");
+    btnEl.style.setProperty("--yPos", y + "px");
+});
